21b5188297f4fc0ef313e2afe8266e983c67964f
mouadedd <mdx.edd@gmail.com>
2024-07-02 06:24:49 -0700

    request

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -2,14 +2,14 @@ import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
 
 class AppController {
-  static getStatus(req, resp) {
-    resp.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+  static getStatus(request, response) {
+    response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
   }
 
-  static async getStats(req, resp) {
+  static async getStats(request, response) {
     const nUser = await dbClient.nbUsers();
-    const nFile = await dbClient.nbFiles();
-    resp.status(200).json({ users: nUser, files: nFile });
+    const nFiles = await dbClient.nbFiles();
+    response.status(200).json({ users: nUser, files: nFile });
   }
 }
 
